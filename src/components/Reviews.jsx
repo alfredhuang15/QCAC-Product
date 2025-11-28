4406a7ba620209e4f2596f805485724a81fd03e3
alfredhuang15 <145942233+alfredhuang15@users.noreply.github.com>
2025-11-28 11:04:47 -0500

    added a interactive star slider for adding reviews

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -3,6 +3,9 @@ import reviews from "../data/sampleReviews.json";
 import { FaStar, FaRegStar, FaStarHalfAlt } from "react-icons/fa";
 
 export default function Reviews() {
+  const [rating, setRating] = React.useState(0);
+  const [hoverRating, setHoverRating] = React.useState(0);
+
   const renderStars = (rating) => {
     const stars = [];
     const totalStars = 5;
@@ -20,9 +23,73 @@ export default function Reviews() {
     return stars;
   };
 
+  const starDisplay = hoverRating || rating;
+
+  const hoverStars = [];
+  for (let i = 1; i <= 5; i++) {
+    const fill = starDisplay;
+
+    const icon = 
+    fill >= i ? (
+      <FaStar color="#ffc107" />
+    ) : fill >= i - 0.5 ? (
+      <FaStarHalfAlt color="#ffc107" />
+    ) : (
+      <FaRegStar color="#ffc107" />
+    );
+
+    hoverStars.push(
+      <div key={i}
+        style={{   position: "relative",
+          width: "28px",
+          height: "28px",
+          display: "inline-block",
+          marginRight: "5px", }}
+        >
+        <span
+          style={{ position: "absolute",
+            left: 0,
+            width: "50%",
+            height: "100%",
+            cursor: "pointer",
+            zIndex: 2}}
+          onMouseEnter={() => setHoverRating(i - 0.5)}
+          onMouseLeave={() => setHoverRating(0)}
+          onClick={() => setRating(i - 0.5)}
+        />
+
+        <span
+          style={{ position: "absolute",
+            right: 0,
+            width: "50%",
+            height: "100%",
+            cursor: "pointer",
+            zIndex: 2, }}
+          onMouseEnter={() => setHoverRating(i)}
+          onMouseLeave={() => setHoverRating(0)}
+          onClick={() => setRating(i)}
+        />
+        <span
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            fontSize: "28px",
+            pointerEvents: "none",
+            color: "#ffc107",
+          }}
+        >{icon}</span>
+      </div>
+    );
+  }
+
   return (
     <section style={{ padding: "20px" }}>
       <h2>Customer Ratings</h2>
+
+      <div>
+        {hoverStars}
+      </div>
 
       <div>
         {reviews.map((review, index) => (
